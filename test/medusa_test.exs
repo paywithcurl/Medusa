c19b43d2738e9e43f61720776e7a1beefbe5ddf9
Thanya Nitithatsanakul <ammbot@gmail.com>
2016-10-21 11:06:45 +0700

    Refactor config
    - for safety adapter configuration

--- test/medusa_test.exs
+++ test/medusa_test.exs
@@ -2,7 +2,23 @@ defmodule MedusaTest do
   use ExUnit.Case
   doctest Medusa
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defp restart_app do
+    Application.stop(:medusa)
+    Application.ensure_all_started(:medusa)
   end
+
+  test "not config should fallback to default" do
+    Application.delete_env(:medusa, Medusa, persistent: true)
+    restart_app()
+    assert Application.get_env(:medusa, Medusa) ==
+      [adapter: Medusa.Adapter.Local]
+  end
+
+  test "config invalid adapter should fallback to Local" do
+    Application.put_env(:medusa, Medusa, [adapter: Wrong], persistent: true)
+    restart_app()
+    assert Application.get_env(:medusa, Medusa) ==
+      [adapter: Medusa.Adapter.Local]
+  end
+
 end
