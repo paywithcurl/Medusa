2f5176b3bf5dbc59dca7fb9eaa4f4e17e0903764
Thanya Nitithatsanakul <ammbot@gmail.com>
2017-02-09 14:37:23 +0700

    change drop_on_failure to on_failure

--- lib/medusa/consumer/rabbitmq.ex
+++ lib/medusa/consumer/rabbitmq.ex
@@ -5,20 +5,23 @@ defmodule Medusa.Consumer.RabbitMQ do
   require Logger
   alias Medusa.Message
 
-  def start_link(args) do
-    to_link =
-      args |> get_in([:opts, :queue_name]) |> String.to_atom |> Process.whereis
-    params = %{
-      function: Keyword.fetch!(args, :function) |> List.wrap,
-      to_link: to_link,
-      opts: Keyword.get(args, :opts, [])
-   }
-    GenStage.start_link(__MODULE__, params)
+  defmodule State do
+    defstruct callback: nil, producer: nil, opts: []
   end
 
-  def init(%{to_link: link} = params) do
-    Logger.debug("Starting #{__MODULE__} for: #{inspect params}")
-    {:consumer, params, subscribe_to: [link]}
+  def start_link(opts) do
+    GenStage.start_link(__MODULE__, opts)
+  end
+
+  def init(opts) do
+    producer = find_producer_pid_from_opts(opts)
+    state = %State{
+      callback: Keyword.fetch!(opts, :function) |> List.wrap,
+      producer: find_producer_pid_from_opts(opts),
+      opts: Keyword.get(opts, :opts, []) |> add_default_options()
+    }
+    Logger.debug("Starting #{__MODULE__} for: #{inspect state}")
+    {:consumer, state, subscribe_to: [producer]}
   end
 
   @doc """
@@ -30,7 +33,7 @@ defmodule Medusa.Consumer.RabbitMQ do
   end
 
   def handle_info({:retry, %Message{} = message}, state) do
-    do_event(message, state.function, message, state)
+    do_event(message, state.callback, message, state)
     {:noreply, [], state}
   end
 
@@ -51,14 +54,13 @@ defmodule Medusa.Consumer.RabbitMQ do
     {:noreply, [], state}
   end
 
-  defp do_handle_events(events, %{function: f, opts: opts} = state) do
+  defp do_handle_events(events, %{callback: f, opts: opts} = state) do
     Enum.each(events, fn event ->
       metadata = event.metadata
       with %AMQP.Channel{} <- metadata["channel"],
            tag when is_number(tag) <- metadata["delivery_tag"],
-           validators <- Keyword.get(opts, :message_validators, []),
-           :ok <- Medusa.validate_message(validators,
-                                          event) do
+           validators = opts.message_validators,
+           :ok <- Medusa.validate_message(validators, event) do
         do_event(event, f, event, state)
       else
         {:error, reason} ->
@@ -66,65 +68,60 @@ defmodule Medusa.Consumer.RabbitMQ do
           {:error, "message is invalid"}
       end
     end)
-    if opts[:bind_once] do
-      {:stop, :normal, state}
-    else
-      {:noreply, [], state}
-    end
+    respone_handle_events(state)
   end
 
-  defp do_event(%Message{} = message, [function], original_message, state) do
+  defp do_event(%Message{} = message, [callback], original_message, state) do
     try do
-      case function.(message) do
+      case callback.(message) do
         :ok -> ack_message(original_message)
         :error ->
-          Logger.error("Error processing message #{inspect function}")
+          Logger.error("Error processing message #{inspect callback}")
           retry_event(original_message, state)
         {:error, reason} ->
-          Logger.error("Error processing message #{inspect function} #{inspect reason}")
+          Logger.error("Error processing message #{inspect callback} #{inspect reason}")
           retry_event(original_message, state)
         error ->
-          Logger.error("Error processing message #{inspect function} #{inspect error}")
+          Logger.error("Error processing message #{inspect callback} #{inspect error}")
           drop_message(original_message)
       end
     rescue
       error ->
-        Logger.error("Error processing message #{inspect function} #{inspect error}")
+        Logger.error("Error processing message #{inspect callback} #{inspect error}")
         retry_event(original_message, state)
     catch
       error ->
-        Logger.error("Error processing message #{inspect function} #{inspect error}")
+        Logger.error("Error processing message #{inspect callback} #{inspect error}")
         retry_event(original_message, state)
       error, other_error ->
-        Logger.error("Error processing message #{inspect function} #{inspect error} #{inspect other_error}")
+        Logger.error("Error processing message #{inspect callback} #{inspect error} #{inspect other_error}")
         retry_event(original_message, state)
     end
   end
 
-  defp do_event(%Message{} = message, [function|tail], original_message, state) do
+  defp do_event(%Message{} = message, [callback|tail], original_message, state) do
     try do
-      case function.(message) do
+      case callback.(message) do
         new = %Message{} -> do_event(new, tail, original_message, state)
         error ->
-          Logger.error("Error processing message #{inspect function} #{inspect error}")
+          Logger.error("Error processing message #{inspect callback} #{inspect error}")
           drop_or_requeue_message(original_message, state)
       end
     rescue
       error ->
-        Logger.error("Error processing message #{inspect function} #{inspect error}")
+        Logger.error("Error processing message #{inspect callback} #{inspect error}")
         drop_or_requeue_message(original_message, state)
     catch
       error ->
-        Logger.error("Error processing message #{inspect function} #{inspect error}")
+        Logger.error("Error processing message #{inspect callback} #{inspect error}")
         drop_or_requeue_message(original_message, state)
       error, other_error ->
-        Logger.error("Error processing message #{inspect function} #{inspect error} #{inspect other_error}")
+        Logger.error("Error processing message #{inspect callback} #{inspect error} #{inspect other_error}")
         drop_or_requeue_message(original_message, state)
     end
   end
 
-  defp retry_event(%Message{} = message, state) do
-    max_retries = state.opts[:max_retries] || 1
+  defp retry_event(%Message{} = message, %{opts: %{max_retries: max_retries}} = state) do
     message = update_in(message.metadata["retry"], &((&1 || 0) + 1))
     if message.metadata["retry"] <= max_retries do
       base = Medusa.config |> Keyword.get(:retry_consume_pow_base, 2)
@@ -136,12 +133,22 @@ defmodule Medusa.Consumer.RabbitMQ do
     end
   end
 
-  defp drop_or_requeue_message(%Message{} = message, state) do
-    if state.opts[:drop_on_failure] do
-      drop_message(message)
-    else
-      requeue_message(message)
-    end
+  defp respone_handle_events(%State{opts: %{bind_once: true}} = state) do
+    {:stop, :normal, state}
+  end
+
+  defp respone_handle_events(%State{opts: %{bind_once: false}} = state) do
+    {:noreply, [], state}
+  end
+
+  defp drop_or_requeue_message(%Message{} = message,
+                               %{opts: %{on_failure: :drop}}) do
+    drop_message(message)
+  end
+
+  defp drop_or_requeue_message(%Message{} = message,
+                               %{opts: %{on_failure: :keep}}) do
+    requeue_message(message)
   end
 
   defp ack_message(%Message{metadata: metadata}) do
@@ -162,4 +169,19 @@ defmodule Medusa.Consumer.RabbitMQ do
                     requeue: false)
   end
 
+  defp find_producer_pid_from_opts(opts) do
+      opts
+      |> get_in([:opts, :queue_name])
+      |> String.to_atom()
+      |> Process.whereis()
+  end
+
+  defp add_default_options(opts) do
+    [message_validators: [],
+     bind_once: false,
+     max_retries: 1,
+     on_failure: :keep]
+    |> Keyword.merge(opts)
+    |> Enum.into(%{})
+  end
 end
--- test/adapter/rabbitmq_adapter_test.exs
+++ test/adapter/rabbitmq_adapter_test.exs
@@ -46,40 +46,40 @@ defmodule Medusa.Adapter.RabbitMQTest do
                          &message_to_test/1,
                          queue_name: "rabbit_retry_failed_test_1",
                          max_retries: 1,
-                         drop_on_failure: true)
+                         on_failure: :drop)
     :ok = Medusa.consume("rabbit.retry.requeue1",
                          &message_to_test/1,
                          queue_name: "rabbit_retry_requeue_test_1",
                          max_retries: 1,
-                         drop_on_failure: false)
+                         on_failure: :keep)
     :ok = Medusa.consume("rabbit.wrong1",
                          &message_to_test/1,
                          queue_name: "rabbit_wrong_test_1",
                          max_retries: 1,
-                         drop_on_failure: false)
+                         on_failure: :keep)
     :ok = Medusa.consume("rabbit.multi1",
                          [&reverse_message/1, &forward_message_to_test/1],
                          queue_name: "rabbit_multi_test_1")
     :ok = Medusa.consume("rabbit.multi.failed1",
                          [&error_message/1, &forward_message_to_test/1],
                          queue_name: "rabbit_multi_failed_test_1",
-                         drop_on_failure: true)
+                         on_failure: :drop)
     :ok = Medusa.consume("rabbit.multi.failed3",
                          [&reverse_message/1, &reverse_message/1],
                          queue_name: "rabbit_multi_failed_test_3",
-                         drop_on_failure: false)
+                         on_failure: :keep)
     :ok = Medusa.consume("rabbit.multi.failed4",
                          [&reverse_message/1, &error_message/1],
                          queue_name: "rabbit_multi_failed_test_4",
-                         drop_on_failure: true)
+                         on_failure: :drop)
     :ok = Medusa.consume("rabbit.multi.failed5",
                          [&reverse_message/1, &message_to_test/1],
                          queue_name: "rabbit_multi_failed_test_5",
-                         drop_on_failure: false)
+                         on_failure: :keep)
     :ok = Medusa.consume("rabbit.multi.failed6",
                          [&reverse_message/1, &message_to_test/1],
                          queue_name: "rabbit_multi_failed_test_6",
-                         drop_on_failure: true)
+                         on_failure: :drop)
     :ok = Medusa.consume("rabbit.validator1",
                          &forward_message_to_test/1,
                          queue_name: "rabbit_validator_test_1")
@@ -245,7 +245,7 @@ defmodule Medusa.Adapter.RabbitMQTest do
   end
 
   describe "Drop message when reach max_retries" do
-    test "setting drop_on_failure retry until reach maximum before drop" do
+    test "setting on_failure to :drop retry until reach maximum before drop" do
       body = random_string()
       publish_test_message("rabbit.retry.failed1", body, %{"times" => 2})
       refute_receive %Message{body: ^body, metadata: %{"event" => "rabbit.retry.failed1"}}, 1_000
@@ -278,17 +278,17 @@ defmodule Medusa.Adapter.RabbitMQTest do
   end
 
   describe "Multi functions in consume wrong failure in the middle" do
-    test "not return %Message{} with drop_on_failure should drop immediately" do
+    test "not return %Message{} with on_failure to :drop should drop immediately" do
       body = random_string()
       publish_test_message("rabbit.multi.failed1", body)
       refute_receive %Message{body: ^body, metadata: %{"event" => "rabbit.multi.failed1"}}, 1_000
     end
 
-    # test "not return %Message{} with no drop_on_failure should requeue" do
+    # test "not return %Message{} with on_failure to :keep should requeue" do
     #   myself = pid_to_list(self())
     #   %{body: body} = publish_consume([&MyModule.state/1, &MyModule.echo/1],
     #                                   %{times: 2, middleware: true, from: myself},
-    #                           drop_on_failure: false)
+    #                           on_failure: :keep)
     #   assert_receive %Message{body: ^body}, 1_000
     # end
   end
@@ -300,19 +300,19 @@ defmodule Medusa.Adapter.RabbitMQTest do
       refute_receive %Message{body: ^body, metadata: %{"event" => "rabbit.multi.failed3"}}, 1_000
     end
 
-    test ":error with drop_on_failure should drop immediately" do
+    test ":error with on_failure to :drop should drop immediately" do
       body = random_string()
       publish_test_message("rabbit.multi.failed4", body)
       refute_receive %Message{body: ^body, metadata: %{"event" => "rabbit.multi.failed4"}}, 1_000
     end
 
-    # test ":error with no drop_on_failure should requeue" do
+    # test ":error with on_failure to :keep should requeue" do
     #   body = random_string()
     #   publish_test_message("rabbit.multi.failed5", body)
     #   assert_receive %Message{body: ^body, metadata: %{"event" => "rabbit.multi.failed5"}}, 1_000
     # end
 
-    test ":error with drop_on_failure should drop immediately after reach max retries" do
+    test ":error with on_failure to :drop should drop immediately after reach max retries" do
       body = random_string()
       publish_test_message("rabbit.multi.failed6", body, %{times: 100})
       refute_receive %Message{body: ^body, metadata: %{"event" => "rabbit.multi.failed6"}}, 1_000
